f6b01e0f9cda5a2ef77a2c74b498c698507b6f5f
fesmjke <fatalerror.sm@gmail.com>
2022-10-29 00:00:37 +0300

    added user interface

---
+++ src/types/IUser.ts
@@ -0,0 +1,15 @@
+export interface IBaseUser {
+    firstName: string;
+}
+
+export interface IUser {
+    firstName: string;
+    lastName?: string;
+    email?: string;
+    homePhone?: string;
+    interests?: string[];
+    isArchived?: boolean;
+    isBuyer?: boolean;
+    isSeller?: boolean;
+    isSpam?: boolean;
+}
