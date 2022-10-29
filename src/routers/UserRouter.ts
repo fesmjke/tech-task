6afbc5b04eb2452f0c8eabf2138c3eb37409cca3
fesmjke <fatalerror.sm@gmail.com>
2022-10-30 01:52:18 +0300

    added new route for find single user by id

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -4,6 +4,7 @@ import UserController from '../controllers/UserController';
 const router = Router();
 
 router.get('/users', UserController.find);
+router.get('/users/:id', UserController.findById);
 router.post('/users', UserController.create);
 router.delete('/users/:id', UserController.delete);
 
