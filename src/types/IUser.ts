dd1633a2367b0eeaffd1866d5f31c6ab581d45c1
fesmjke <fatalerror.sm@gmail.com>
2022-10-29 17:23:25 +0300

    removed base user interface, added request interface

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -1,15 +1,21 @@
-export interface IBaseUser {
-	firstName: string;
-}
-
 export interface IUser {
+	_id: string;
 	firstName: string;
-	lastName?: string;
-	email?: string;
-	homePhone?: string;
-	interests?: string[];
+	lastName: string;
+	name: string;
+	email: string;
+	homePhone: string;
+	interests: string[];
 	isArchived?: boolean;
 	isBuyer?: boolean;
 	isSeller?: boolean;
 	isSpam?: boolean;
+}
+
+export interface IRequestUser {
+	firstName: string;
+	lastName: string;
+	email?: string;
+	homePhone?: string;
+	interests: string[];
 }
