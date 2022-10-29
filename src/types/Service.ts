11fbdf694bc08f0a1a69d9eb3de8367c053e66aa
fesmjke <fatalerror.sm@gmail.com>
2022-10-30 01:45:47 +0300

    added parameter to method

--- src/types/Service.ts
+++ src/types/Service.ts
@@ -2,14 +2,14 @@ import type {IRequestUser, IUser} from './IUser';
 
 export interface IService {
 	find(): Promise<IUser[]>;
-	findOne(): Promise<IUser>;
+	findOne(id: string): Promise<IUser>;
 	create(object: IRequestUser): Promise<IUser>;
 	delete(id: string): Promise<boolean>;
 }
 
 export abstract class Service implements IService {
 	abstract find(): Promise<IUser[]>;
-	abstract findOne(): Promise<IUser>;
+	abstract findOne(id: string): Promise<IUser>;
 	abstract create(object: IRequestUser): Promise<IUser>;
 	abstract delete(id: string): Promise<boolean>;
 }
