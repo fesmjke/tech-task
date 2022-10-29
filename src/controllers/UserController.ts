7166729595111411c72effd2584b7344a02d7425
fesmjke <fatalerror.sm@gmail.com>
2022-10-30 01:51:51 +0300

    added implementation for user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,10 @@
 import type {Request, Response, NextFunction} from 'express';
+import {ValidationError} from 'joi';
 import type {UserService} from '../services/UserService';
 import {default as userService} from '../services/UserService';
 import {Controller} from '../types/Controller';
 import type {IRequestUser} from '../types/IUser';
+import {userRequestSchema} from '../validation/user.validation';
 
 class UserController extends Controller {
 	private readonly _userService: UserService;
@@ -15,23 +17,54 @@ class UserController extends Controller {
 	find = async (request: Request, responce: Response, next: NextFunction) => {
 		try {
 			const users = await this._userService.find();
-		} catch (e) {
-			responce.status(501).send('Not implimented');
+			responce.status(200).send(users);
+		} catch (e: any) {
+			responce.status(404).send({error: e.message});
+		}
+	};
+
+	findById = async (request: Request, responce: Response, next: NextFunction) => {
+		const {id} = request.params;
+
+		try {
+			const user = await this._userService.findOne(id);
+			responce.status(200).send(user);
+		} catch (e: any) {
+			responce.status(404).send({error: e.message});
 		}
 	};
 
 	create = async (request: Request, responce: Response, next: NextFunction) => {
 		const user: IRequestUser = request.body;
 
-		console.log('incoming user', user.firstName);
-		
-		const created = await this._userService.create(user);
+		try {
+			await userRequestSchema.validateAsync(user);
+		} catch (e: any) {
+			if (e instanceof ValidationError) {
+				const [message] = e.details;
+				responce.status(400).send({error: message.message});
+				next();
+				return;
+			}
+		}
 
-		responce.status(200).send(created);
+		try {
+			const created = await this._userService.create(user);
+			responce.status(200).send(created);
+		} catch (e: any) {
+			responce.status(400).send({error: e.message});
+		}
 	};
 
 	delete = async (request: Request, responce: Response, next: NextFunction) => {
-		responce.status(501).send('Not implimented');
+		const {id} = request.params;
+
+		try {
+			await this._userService.delete(id);
+			responce.status(200).send(`Successfully deleted user with id ${id}`);
+		} catch (e: any) {
+			responce.status(400).send({error: e.message});
+		}
 	};
 }
 
