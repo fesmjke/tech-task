ecf9d294360ed861efea5770297e2f96628e422c
fesmjke <fatalerror.sm@gmail.com>
2022-10-31 01:22:04 +0200

    fixed bug when after error triggered a not found middleware

--- src/middleware/not-found.middleware.ts
+++ src/middleware/not-found.middleware.ts
@@ -2,6 +2,10 @@ import type {Request, Response, NextFunction} from 'express';
 import {errorLogger} from '../logger/logger';
 
 export const notFound = (request: Request, responce: Response, next: NextFunction) => {
+	if(responce.headersSent){
+		next();
+		return;
+	}
 	responce.status(404);
 
 	responce.send({error: `Cannot ${request.method} ${request.path}`});
