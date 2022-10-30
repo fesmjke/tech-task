d09a8adb72b76aaf41740b834fc20bbeab6a45c4
fesmjke <fatalerror.sm@gmail.com>
2022-10-30 21:48:57 +0200

    added not found middleware answer

---
+++ src/middleware/not-found.middleware.ts
@@ -0,0 +1,12 @@
+import type {Request, Response, NextFunction} from 'express';
+import { errorLogger } from '../logger/logger';
+
+export const notFound = (request: Request, responce: Response, next: NextFunction) => {
+    responce.status(404);
+
+    responce.send({error : `Cannot ${request.method} ${request.path}`});
+    errorLogger.error(`Cannot ${request.method} ${request.path}`);
+    
+    next();
+    return;
+}
