45920673f7db85bdd69d33b49c3a636fcbf63713
fesmjke <fatalerror.sm@gmail.com>
2022-10-30 17:32:56 +0200

    added config to load server env variables

---
+++ src/config/server.config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+export const loadServerConfigAsync = async () => {
+    dotenv.config();
+
+	const config = {
+		server: {
+			port: process.env.PORT ?? '3000',
+		},
+	};
+
+    return config;
+}
