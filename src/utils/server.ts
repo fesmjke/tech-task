7442abcf4819f5c32990c22ea7fdf95e6aa55735
fesmjke <fatalerror.sm@gmail.com>
2022-10-30 21:49:49 +0200

    added not found middleware to create server function

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,13 @@
 import {Application} from '../application/Application';
+import { notFound } from '../middleware/not-found.middleware';
 import UserRouter from '../routers/UserRouter';
 
 export const createServer = (port: string) => {
 	const app = new Application(port);
 
 	app.attachRouter(UserRouter);
+
+	app.attachMiddleware(notFound);
 
 	return app;
 };
